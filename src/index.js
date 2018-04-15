fbe12511fdf8384acd479ed148ba60d844e8d854
Kotaro Mochizuki <mochizuki@basicinc.jp>
2018-04-15 13:58:30 +0900

    Add lits

--- src/index.js
+++ src/index.js
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+class TodoApp extends Component {
+  render () {
+    const todos = [
+      {name: 'hoge'},
+      {name: 'piyo'}
+    ]
+    return (
+      <TodoList todos={todos} />
+    )
+  }
+}
+
+const TodoList = (props) => {
+  return (
+    <table>
+      <tr><th>やること</th></tr>
+      {props.todos.map((todo) =>
+        <TodoItem todo={todo} key={todo.name} />
+      )}
+    </table>
+  )
+}
+TodoList.propTypes = {
+  todos: PropTypes.array
+}
+
+const TodoItem = (props) => {
+  const name = props.todo.name
+  return (
+    <tr>
+      <td>{name}</td>
+    </tr>
+  )
+}
+TodoItem.propTypes = {
+  todo: PropTypes.object
+}
 
 ReactDOM.render(
-  <h1>Hello</h1>,
+  <TodoApp />,
   document.getElementById('root')
 )
